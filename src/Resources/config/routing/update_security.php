8f8c52dd59810b3f25ea3e2b9c1e55feda472a1a
core23 <mail@core23.de>
2023-08-23 22:46:23 +0200

    Fix routing

--- src/Resources/config/routing/update_security.php
+++ src/Resources/config/routing/update_security.php
@@ -14,10 +14,20 @@ declare(strict_types=1);
 namespace Symfony\Component\Routing\Loader\Configurator;
 
 use Nucleos\UserBundle\Action\UpdateSecurityAction;
+use Symfony\Bundle\FrameworkBundle\Controller\RedirectController;
 
 return static function (RoutingConfigurator $routes): void {
-    $routes->add('nucleos_user_update_security', '/change-password')
+    $routes->add('nucleos_user_update_security', '/update-security')
         ->controller(UpdateSecurityAction::class)
         ->methods(['GET', 'POST'])
+    ;
+
+    $routes->add('nucleos_user_update_security_legacy', '/change-password')
+        ->controller(RedirectController::class)
+        ->methods(['GET', 'POST'])
+        ->defaults([
+            'route'     => 'nucleos_user_update_security',
+            'permanent' => true,
+        ])
     ;
 };
