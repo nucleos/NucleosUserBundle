f3f25c8540b178d0d526a45729c9555ba4078e82
core23 <mail@core23.de>
2024-08-14 17:43:14 +0200

    Fix symfony deprecation

--- src/DependencyInjection/NucleosUserExtension.php
+++ src/DependencyInjection/NucleosUserExtension.php
@@ -22,9 +22,9 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Config\Loader\FileLoader;
 use Symfony\Component\DependencyInjection\Alias;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 final class NucleosUserExtension extends Extension
 {
