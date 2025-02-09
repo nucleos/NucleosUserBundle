e58ef5cfb4a286cda26e22f88041fb8300aaa7d8
core23 <mail@core23.de>
2025-02-09 17:52:39 +0100

    Fix CS

--- src/Resources/config/flash_notifications.php
+++ src/Resources/config/flash_notifications.php
@@ -1,8 +1,6 @@
 <?php
 
 /*
- * This file is part of the NucleosUserBundle package.
- *
  * (c) Christian Gripp <mail@core23.de>
  *
  * For the full copyright and license information, please view the LICENSE
