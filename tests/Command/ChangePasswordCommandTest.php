3711fde764df09890130e8613a5ac6462eee15a6
core23 <mail@core23.de>
2025-02-09 17:52:39 +0100

    Fix CS

--- tests/Command/ChangePasswordCommandTest.php
+++ tests/Command/ChangePasswordCommandTest.php
@@ -3,8 +3,6 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the NucleosUserBundle package.
- *
  * (c) Christian Gripp <mail@core23.de>
  *
  * For the full copyright and license information, please view the LICENSE
